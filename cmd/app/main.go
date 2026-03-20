e7996128d9cab18c0461977c32cff816ce189768
ibezgin <kurt1v@yandex.ru>
2026-03-20 11:33:28 +0300

    Remove unused import from `main.go`

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,14 +8,11 @@ package main
 // @produce json
 
 import (
-	"fmt"
-
 	"github.com/mobqom/questions/config"
 	"github.com/mobqom/questions/internal/server"
 )
 
 func main() {
-	fmt.Println("Starting server...")
 	cfg := config.Init()
 	server.Run(cfg)
 }
